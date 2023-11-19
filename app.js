cfd7875c7fd00ac39c0491ce8d50b972813d6e0e
Shreyash Daundkar <productiveappa@gmail.com>
2023-11-19 11:24:46 +0530

    move ts into src folder

--- app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const express = require('express');
-const bodyParser = require('body-parser');
-const todoRoute = require('./routes/todo');
-const app = express();
-app.use(bodyParser.json());
-app.use('/todo', todoRoute);
-app.listen(3000);
--- routes/todo.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const { Router } = require('express');
-const { displayTodos, addTodos, deleteTodos, editTodos } = require('../controllers/todo');
-const router = Router();
-router.get('/', displayTodos);
-router.post('/', addTodos);
-router.delete('/', deleteTodos);
-router.put('/', editTodos);
-module.exports = router;
